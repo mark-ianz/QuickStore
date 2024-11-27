2a4955d8dd10eb13561344a3614265f23782fac1
mark-ianz <bustillomarkian23@gmail.com>
2024-11-27 20:58:08 +0800

    removed the required field on name and email making it optional

--- Feedback.js
+++ Feedback.js
@@ -3,11 +3,9 @@ import mongoose from "mongoose";
 const feedbackSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
   },
   email: {
     type: String,
-    required: true,
   },
   message: {
     type: String,
