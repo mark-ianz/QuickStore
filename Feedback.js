42584897e1699ade95c4c934a61e9a1d040112f5
mark-ianz <bustillomarkian23@gmail.com>
2024-11-27 20:23:40 +0800

    added from

--- Feedback.js
+++ Feedback.js
@@ -13,6 +13,10 @@ const feedbackSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  from: {
+    type: String,
+    required: true
+  },
   createdAt: {
     type: Date,
     default: Date.now,
